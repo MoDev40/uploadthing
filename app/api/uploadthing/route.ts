a0ee2baad3f91eabd14727633bac9e6a47550e52
M40 <modev.404@gmail.com>
2024-07-21 13:29:27 +0300

    delete files  uploadthing

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,6 +1,8 @@
 import { createRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
+import { NextRequest, NextResponse } from "next/server";
+import { utApi } from "@/app/config/uploadthing";
  
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
@@ -9,3 +11,13 @@ export const { GET, POST } = createRouteHandler({
   // Apply an (optional) custom config:
   // config: { ... },
 });
+
+export async function DELETE(req:NextRequest){
+  try {
+      const { fileKeys } = await req.json()
+      await utApi.deleteFiles(fileKeys)
+      return NextResponse.json("ok",{status:200})
+  } catch (error) {
+      return NextResponse.json({message:"unexpected error"},{status:500})
+  }
+}
